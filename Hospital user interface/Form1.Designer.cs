4aa5934be8714302921362f351a4bd4eb8502ce9
Skell25 <SH1836@canterbury.ac.uk>
2026-03-05 11:22:17 +0000

    Work that ive done regarding the email and password login credentials

---
+++ Hospital user interface/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Hospital_user_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMAIL";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PASSWORD";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(235, 64);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(147, 20);
+            this.EmailTextBox.TabIndex = 2;
+            this.EmailTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(236, 121);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(147, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            this.PasswordTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(300, 187);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(93, 29);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(163, 187);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 29);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 423);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Hospital user interface/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospital_user_interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string Email;
+            string Password;
+        }
+        //btw if the UI form shows an error symbol when you exit and open up the project again, just go to where the project files are located and delete the obj and bin files, the form should show after that
+
+        private void label1_Click(object sender, EventArgs e) //This section of code is jsut for the label named "EMAIL" this can be ignored
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) //The same is applied here except it is for "PASSWORD" label
+        {
+
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e) //This section of code is for the email textbox
+        {
+            string Email = Console.ReadLine(); 
+            
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e) // this section of code is for the password text box, an idea of development is to 'censor the text shown in the password box'
+        {
+            string Password = Console.ReadLine();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e) //code for the exit button, this is sued when the user wants to leave the login portal without entering any details
+        {
+            this.Close(); //this is tested and it works
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e, string Email, string Password) //code for the login button, this might be very long
+        {
+            //this has not been tested yet, all just theory
+
+
+            if (Email == Email || Password == Password) // variable scoping fixed, but currently trying to find a better comparison to check if the login credentials ae correct
+            {
+                MessageBox.Show("Invald Email or Password");
+            }
+            else //further ideas for development can be to make an else if statement for just email and password 
+            {
+                MessageBox.Show("Login Successful"); //if successful should give show a message prompt that the login was successful
+
+                this.Hide();// should give an option to close the messagebox prompt
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //this is the code for the UI form
+        {
+
+        }
+    }
+}
